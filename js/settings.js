21f2513be156bf91d5d497402e133e2b34e27b0e
ElenaMedvedeva <elena.medvedeva@duallab.com>
2013-02-05 00:46:01 +0100

    Contact list is printed after Authenticate button in Settings is pressed

--- js/settings.js
+++ js/settings.js
@@ -18,15 +18,21 @@ $(document).ready(function() {
 	  });
 	 
 	 $('#btnAuthenticate').bind('click', function() {
-		 storer.getSettings(function (data) {
-		 console.log("Settings!!!! ", data);
-		 contacManager.permissions = data;		     
-		 contacManager.getContacts(printContactNames);
-		 console.log("contacManager.contactService",contacManager.contactService);				        
-		}, function (err) {		    	
-			$("#noSettings").text("No settings found!");
-			        console.log("Error:  " + err);
-		});      
+		 storer.getSettings(function(data) {
+				
+				console.log("Settings!!!! ", data);
+				contacManager.permissions = data;
+
+				contacManager.getContacts(printContactNames);
+				console.log("contacManager.contactService",
+						contacManager.contactService);
+
+			}, function(err) {
+
+				$("#contactList").text("No settings found!");
+				$('#noSettingsMessage').show();
+				console.log("Error:  " + err);
+			});
 			     
 	});
 	 
