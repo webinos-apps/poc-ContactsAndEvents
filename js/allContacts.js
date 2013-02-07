f8a9adee04be9dc54272ebf65cf788a5ce0174ae
ElenaMedvedeva <elena.medvedeva@duallab.com>
2013-02-07 13:32:21 +0100

    UI changes for chat, No settings message changed

--- js/allContacts.js
+++ js/allContacts.js
@@ -16,7 +16,7 @@ $(document).ready(
 
 			}, function(err) {
 
-				$("#contactList").text("No settings found!");
+				$("#contactList").text('');
 				$('#noSettingsMessage').show();
 				console.log("Error:  " + err);
 			});
